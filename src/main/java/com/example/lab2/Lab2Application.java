4af71f8200d566b3deb17e6d4b577df29d6698a5
Hrystyna Yaskulska <yaskulskahrystyna@gmail.com>
2023-12-07 02:12:44 +0200

    Remove unnecessary files from project

--- src/main/java/com/example/lab2/Lab2Application.java
+++ src/main/java/com/example/lab2/Lab2Application.java
@@ -1,11 +1,7 @@
 package com.example.lab2;
 
-import com.example.lab2.repository.RatingRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
 public class Lab2Application {
