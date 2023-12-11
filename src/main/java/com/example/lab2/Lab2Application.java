26c119511aee572b206db16564f4524d5833608d
Hrystyna Yaskulska <yaskulskahrystyna@gmail.com>
2023-12-12 00:11:25 +0200

    Add Oracle DB configuration and driver

--- src/main/java/com/example/lab2/Lab2Application.java
+++ src/main/java/com/example/lab2/Lab2Application.java
@@ -3,8 +3,13 @@ package com.example.lab2;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import oracle.jdbc.pool.OracleDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -20,6 +25,16 @@ public class Lab2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Lab2Application.class, args);
+    }
+    @Bean
+    DataSource dataSource() throws SQLException {
+        OracleDataSource dataSource = new OracleDataSource();
+        dataSource.setUser("KHRYSTYNA");
+        dataSource.setPassword("KHRYSTYNA");
+        dataSource.setURL("jdbc:oracle:thin:@localhost:1521:xe");
+        dataSource.setImplicitCachingEnabled(true);
+        // dataSource.setFastConnectionFailoverEnabled(true);
+        return dataSource;
     }
 
 }
